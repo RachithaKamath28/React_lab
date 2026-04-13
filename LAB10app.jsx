1729dfb20ab3504af808131b8a0fc04c192d07f3
Rachitha Kamath <rachitha.23cs109@sode-edu.in>
2026-04-13 12:57:48 +0530

    Import App.css in LAB10app.jsx

--- LAB10app.jsx
+++ LAB10app.jsx
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react';
+import './App.css';
 const DataFetcher=() =>{
   const [data,setData]=useState([]),[q,setQ]=useState(''),[err,setErr]=useState(null),[load,setLoad]=useState(false);
 
